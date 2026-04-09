65e080bbab480f71d309dfcebc17b1db8d155028
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2026-04-09 17:31:31 -0400

    update

--- framework_handler/app.py
+++ framework_handler/app.py
@@ -13,22 +13,24 @@ def spawn_worker(framework_name,api_key, user_query):
 
     # 2. Run the baked image
     container = client.containers.run(
-        image="universal-worker:v1",  # Use the exact name you baked
+        image="universal-worker:v3",  # Use the exact name you baked
         
         # 3. Inject the "Soul" (Variables)
         environment={
             "FRAMEWORK_TYPE": framework_name,
             "GOOGLE_API_KEY": api_key,
-            "USER_QUERY": user_query
+            "USER_QUERY": user_query,
+            "REDIS_HOST": "redis_storage"
         },
         
         # 4. Networking & Cleanup
         detach=True,                    # Run in background
-        #auto_remove=True                # Automatically delete when done (no orphans!)
+        auto_remove=True                # Automatically delete when done (no orphans!)
     )
     hashed_containers[container.id] = container
     print(f"Handler: Spawned container {container.id} for {framework_name}")
     #return container.id
+    print(hashed_containers)
 
 async def main(): #Input ENV: FRAMEWORK_LIST, GOOGLE_API_KEY, USER_QUERY
     framework_list = os.getenv("FRAMEWORK_LIST", "null").split(",")
--- generic_framework/frameworks/langchain_logic.py
+++ generic_framework/frameworks/langchain_logic.py
@@ -1,14 +1,26 @@
 
 import os
 import asyncio
+
+from flask import json
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
 import importlib.util
+import redis
 
+def redis_connect():
+    REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+    r = redis.Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
 
+    try:
+        r.set("system_status", "ready")
+        print(f"Connected to Redis at {REDIS_HOST}!")
+        run_task("What is the capital of France?", r)
+    except redis.ConnectionError:
+        print("Could not connect to Redis. Is the service name correct?")
 
 #input env - USER_QUESTION, GOOGLE_API_KEY
-async def run_task(user_query = "What is the capital of France?"):
+async def run_task(user_query = "What is the capital of France?",redis_object = None):
     # 1. Grab the question from the Docker environment
     if not user_query:
         print("No user query provided.")
@@ -22,16 +34,26 @@ async def run_task(user_query = "What is the capital of France?"):
     # 3. Initialize the model based on provider
     
     # Use Gemini 2.5 Flash model
-    model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=google_api_key)
+    #model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=google_api_key)
 
-    messages = [
-        SystemMessage(content="You are a concise AI assistant."),
-        HumanMessage(content=user_query)
-    ]
+    # messages = [
+    #     SystemMessage(content="You are a concise AI assistant."),
+    #     HumanMessage(content=user_query)
+    # ]
 
     # 4. Get the response
-    response = await model.ainvoke(messages)
-    print(f"\nAI Response:\n{response.content}")
+    # response = await model.ainvoke(messages)
+    # print(f"\nAI Response:\n{response.content}")
+    task_packet = {
+        "user_query": user_query,
+        "test": "testing"
+    }
+    
+    # LPUSH adds the item to the list named 'benchmark_tasks'
+    # It returns the new length of the list
+    queue_length = redis_object.lpush("test_tasks", json.dumps(task_packet))
+
+    
 
 if __name__ == "__main__":
-    asyncio.run(run_task())
+    asyncio.run(redis_connect())
